2f8a669caa258dd4137c0fa4c8355291af139c2c
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-22 11:20:22 +0330

    [ADD] playground.js: add setup for sum

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,15 +1,21 @@
 /** @odoo-module **/
 
 import {Component, useState} from "@odoo/owl";
-import {Counter} from "./counter/counter";
 import {Card} from "./card/card";
 const {markup, xml} = owl;
+import {Counter} from "./counter/counter";
 
 export class Playground extends Component {
 	static template = "awesome_owl.playground";
-	static components = {Card};
+	static components = {Card , Counter};
 	static props = {};
 
+	setup(){
+		this.state = useState({sum: 2 })
+	}
+	incrementSum(){
+		this.state.sum ++;
+	}
 	value1 = "<div>some text 1</div>";
 	value2 = markup("<div>some text 2</div>");
 }
