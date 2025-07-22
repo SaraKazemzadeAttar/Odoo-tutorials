d19b231a2f0e30d3e0d42009ca18e2e5a730d50e
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-22 16:43:36 +0330

    [ADD] playground.js: add addtodo function to allow user to add

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -2,25 +2,36 @@
 
 import {Component, useState} from "@odoo/owl";
 import {Card} from "./card/card";
+
 const {markup, xml} = owl;
 import {Counter} from "./counter/counter";
 import {TodoItem} from "./TodoList/todo_item";
 
 export class Playground extends Component {
 	static template = "awesome_owl.playground";
-	static components = {Card , Counter , TodoItem};
+	static components = {Card, Counter, TodoItem};
 	static props = {};
 
-	setup(){
-		this.state = useState({sum: 2 })
+	setup() {
+		this.state = useState({sum: 2})
+		this.idCounter = 1;
 	}
-	incrementSum(){
-		this.state.sum ++;
+
+	incrementSum() {
+		this.state.sum++;
 	}
+
 	value1 = "<div>some text 1</div>";
 	value2 = markup("<div>some text 2</div>");
-	todos = useState([
-			{id: 2, description: "write tutorial", isCompleted: true},
-			{id: 3, description: "buy milk", isCompleted: false}
-		])
+	todos = useState([]);
+
+	addTodo(ev) {
+		if (ev.keyCode === 13) { // enter
+			const value = ev.target.value.trim();
+			if (value) {
+				this.todos.push({id: this.idCounter++, description: value});
+				ev.target.value = "";
+			}
+		}
+	}
 }
