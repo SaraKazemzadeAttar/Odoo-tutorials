8b394020e81026b33bb599c6083c4b310d507018
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-22 15:18:11 +0330

    [ADD] playground.js: add todo list

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -4,10 +4,11 @@ import {Component, useState} from "@odoo/owl";
 import {Card} from "./card/card";
 const {markup, xml} = owl;
 import {Counter} from "./counter/counter";
+import {TodoItem} from "./TodoList/todo_item";
 
 export class Playground extends Component {
 	static template = "awesome_owl.playground";
-	static components = {Card , Counter};
+	static components = {Card , Counter , TodoItem};
 	static props = {};
 
 	setup(){
@@ -18,4 +19,8 @@ export class Playground extends Component {
 	}
 	value1 = "<div>some text 1</div>";
 	value2 = markup("<div>some text 2</div>");
+	todos = useState([
+			{id: 2, description: "write tutorial", isCompleted: true},
+			{id: 3, description: "buy milk", isCompleted: false}
+		])
 }
