e5e9874e891fe0506ba4a3a6d5b1b84e61d49343
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-22 10:30:34 +0330

    [ADD] playground.js: Update Playground to import markup

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,11 +1,15 @@
 /** @odoo-module **/
 
-import { Component, useState } from "@odoo/owl";
-import { Counter } from "./counter/counter";
-import { Card } from "./card/card";
+import {Component, useState} from "@odoo/owl";
+import {Counter} from "./counter/counter";
+import {Card} from "./card/card";
+const {markup, xml} = owl;
 
 export class Playground extends Component {
-  static template = "awesome_owl.playground";
-  static components = { Card };
-  static props = {};
+	static template = "awesome_owl.playground";
+	static components = {Card};
+	static props = {};
+
+	value1 = "<div>some text 1</div>";
+	value2 = markup("<div>some text 2</div>");
 }
