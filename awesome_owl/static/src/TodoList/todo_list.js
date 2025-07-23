2cc3dd5408b9a5007cd5f33f9f26db4b66e8ec31
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-23 10:06:29 +0330

    [ADD] todo_list.js: add setup

--- awesome_owl/static/src/TodoList/todo_list.js
+++ awesome_owl/static/src/TodoList/todo_list.js
@@ -2,6 +2,11 @@ import { Component, useState} from "@odoo/owl";
 
 
 export class TodoList extends Component {
-
-
+	setup() {
+	   this.myRef = useRef('inputTodo');
+	   onMounted(() => {
+	      console.log(this.myRef.el);
+		  this.myRef.el.focus();
+	   });
+	}
 }
