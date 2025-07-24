cb52930279f62a8f99c162074af6a78139d4729a
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-24 11:30:34 +0330

    [FIX] dashboard_item.js: fix a typo mistake.

--- awesome_dashboard/static/src/dashboard_item/dashboard_item.js
+++ awesome_dashboard/static/src/dashboard_item/dashboard_item.js
@@ -1,6 +1,6 @@
 import { Component } from "@odoo/owl";
 
-export class Dashboard_item extends Component{
+export class DashboardItem extends Component{
 	static template = "awesome_dashboard.item";
 	static props = {
 		size: {type: Number , default: 1}
