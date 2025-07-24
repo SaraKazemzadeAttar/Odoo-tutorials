0539b20dc4f9d7ec6d1fc504c3f56d47ad2f2f16
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-24 12:52:24 +0330

    [REF] dashboard.js/xml : call the rpc and add statics

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -1,17 +1,40 @@
 /** @odoo-module **/
 
-import {Component} from "@odoo/owl";
+import {Component, onWillStart, useState} from "@odoo/owl";
 import {registry} from "@web/core/registry";
 import {Layout} from "@web/search/layout";
 import {useService} from "@web/core/utils/hooks";
 import {DashboardItem} from "../dashboard_item/dashboard_item";
+import {rpc} from "@web/core/network/rpc";
 
 export class AwesomeDashboard extends Component {
 	static template = "awesome_dashboard.AwesomeDashboard";
 	static components = {Layout, DashboardItem};
 
 	setup() {
-		this.action = useService("action")
+		this.action = useService("action");
+
+		this.state = useState({
+			stats: {
+				new_orders: 0,
+				total_amount: 0,
+				avg_tshirt_per_order: 0,
+				cancelled_orders: 0,
+				avg_processing_time: "",
+			},
+		});
+
+		onWillStart(async () => {
+			const result = await rpc("/awesome_dashboard/statistics");
+
+			this.state.stats = {
+				new_orders: result.nb_new_orders,
+				total_amount: result.total_amount,
+				avg_tshirt_per_order: result.average_quantity,
+				cancelled_orders: result.nb_cancelled_orders,
+				avg_processing_time: result.average_time,
+			};
+		});
 	}
 
 	openSettings() {
