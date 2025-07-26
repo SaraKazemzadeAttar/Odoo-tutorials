20588d2a304108345bc6741f4b20a19b06c820bd
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-26 12:19:07 +0330

    [FIX] dashboard_item.js: add slots as a props

--- awesome_dashboard/static/src/dashboard/dashboard_item/dashboard_item.js
+++ awesome_dashboard/static/src/dashboard/dashboard_item/dashboard_item.js
@@ -3,6 +3,10 @@ import { Component } from "@odoo/owl";
 export class DashboardItem extends Component{
 	static template = "awesome_dashboard.item";
 	static props = {
-		size: {type: Number , default: 1}
+		size: {type: Number , default: 1},
+		slots: {
+			type: Object,
+			shape: {default: true},
+		},
 	}
 }
