89f180285cea31944f58b3733a290bafb67627df
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-26 17:18:25 +0330

    [FIX] __manifest__.py

--- awesome_dashboard/__manifest__.py
+++ awesome_dashboard/__manifest__.py
@@ -1,34 +1,48 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Awesome Dashboard",
+	'name': "Awesome Dashboard",
 
-    'summary': """
+	'summary': """
         Starting module for "Discover the JS framework, chapter 2: Build a dashboard"
     """,
 
-    'description': """
+	'description': """
         Starting module for "Discover the JS framework, chapter 2: Build a dashboard"
     """,
 
-    'author': "Odoo",
-    'website': "https://www.odoo.com/",
-    'category': 'Tutorials/AwesomeDashboard',
-    'version': '0.1',
-    'application': True,
-    'installable': True,
-    'depends': ['base', 'web', 'mail', 'crm'],
+	'author': "Odoo",
+	'website': "https://www.odoo.com/",
+	'category': 'Tutorials/AwesomeDashboard',
+	'version': '0.1',
+	'application': True,
+	'installable': True,
+	'depends': ['base', 'web', 'mail', 'crm'],
 
-    'data': [
-        'views/views.xml',
-    ],
-    'assets': {
-        'web.assets_backend': [
-            'awesome_dashboard/static/src/**/*',
-        ],
-	    'awesome_dashboard.dashboard': [
-		    'awesome_dashboard/static/src/dashboard_loader.js',
-		    'awesome_dashboard/static/src/dashboard_action.js'
-	    ]
-    },
-    'license': 'AGPL-3'
+	'data': [
+		'views/views.xml',
+	],
+	'assets': {
+		'web.assets_backend': [
+			'awesome_dashboard/static/src/**/*',
+			('remove', 'awesome_dashboard/static/src/dashboard/**/*'),
+			# 'awesome_dashboard/static/src/dashboard_action.js',
+			# 'awesome_dashboard/static/src/dashboard_loader.js',
+		],
+		'awesome_dashboard.dashboard': [
+			'awesome_dashboard/static/src/dashboard/**/**',
+
+		],
+	},
+	# {
+	#
+	#     'web.assets_backend': [
+	#         'awesome_dashboard/static/src/**/*',
+	#     ],
+	#     'awesome_dashboard.dashboard': [
+	#        'web.core/lazy_component',
+	# 	    'awesome_dashboard/static/src/dashboard_loader.js',
+	# 	    'awesome_dashboard/static/src/dashboard_action.js'
+	#     ]
+	# },
+	'license': 'AGPL-3'
 }
