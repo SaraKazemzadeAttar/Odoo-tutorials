d322f27d872065df4786f9fd78c4a8e4cb1a868c
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-26 12:22:05 +0330

    [ADD] statistics_service.js:Update the statistics service to reload data every 10 minutes (to test it, use 10s instead!)

--- awesome_dashboard/static/src/dashboard/statistics_service.js
+++ awesome_dashboard/static/src/dashboard/statistics_service.js
@@ -1,12 +1,36 @@
-import { rpc } from "@web/core/network/rpc";
-import { memoize } from "@web/core/utils/functions";
+import {rpc} from "@web/core/network/rpc";
+import {reactive} from "@odoo/owl";
+import {registry} from "@web/core/registry";
 
 export const StatisticsService = {
-    start() {
-        return {
-            loadStatistics: memoize(async () => {
-                return await rpc("/awesome_dashboard/statistics");
-            }),
-        };
-    },
+	start() {
+		const stats = reactive({
+			new_orders: 10,
+			total_amount: 0,
+			avg_tshirt_per_order: 0,
+			cancelled_orders: 0,
+			avg_processing_time: "",
+			orders_by_size: {}
+		});
+
+		async function fetchStatistics() {
+			const result = await rpc("/awesome_dashboard/statistics");
+			console.log(stats.new_orders)
+			stats.new_orders = result.nb_new_orders;
+			stats.total_amount = result.total_amount;
+			stats.avg_tshirt_per_order = result.average_quantity;
+			stats.cancelled_orders = result.nb_cancelled_orders;
+			stats.avg_processing_time = result.average_time;
+			stats.orders_by_size = result.orders_by_size;
+		}
+
+		debugger
+		fetchStatistics();
+		// setInterval(fetchStatistics, 10000);
+
+		return {
+			stats,
+		};
+	},
 };
+registry.category("services").add("awesome_dashboard.statistics", StatisticsService);
