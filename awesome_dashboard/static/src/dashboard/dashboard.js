3129c773e930fac7412803192d748c5d1dc80573
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-26 12:21:06 +0330

    [IMP] dashboard.js: call the statistics_service and move stats there

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -5,7 +5,6 @@ import {registry} from "@web/core/registry";
 import {Layout} from "@web/search/layout";
 import {useService} from "@web/core/utils/hooks";
 import {DashboardItem} from "./dashboard_item/dashboard_item";
-import {StatisticsService} from "./statistics_service";
 import {PieChart} from "./piechart/piechart";
 
 export class AwesomeDashboard extends Component {
@@ -14,30 +13,8 @@ export class AwesomeDashboard extends Component {
 
 	setup() {
 		this.action = useService("action");
-		this.statisticsService = useService("awesome_dashboard.statistics");
-
-		this.state = useState({
-			stats: {
-				new_orders: 0,
-				total_amount: 0,
-				avg_tshirt_per_order: 0,
-				cancelled_orders: 0,
-				avg_processing_time: "",
-				orders_by_size: {}
-			},
-		});
-
-		onWillStart(async () => {
-			const result = await this.statisticsService.loadStatistics();
-			this.state.stats = {
-				new_orders: result.nb_new_orders,
-				total_amount: result.total_amount,
-				avg_tshirt_per_order: result.average_quantity,
-				cancelled_orders: result.nb_cancelled_orders,
-				avg_processing_time: result.average_time,
-				orders_by_size: result.orders_by_size,
-			};
-		});
+		const service = useService("awesome_dashboard.statistics");
+        this.state = useState(service.stats);
 	}
 
 	openSettings() {
@@ -65,5 +42,4 @@ export class AwesomeDashboard extends Component {
 	items_list = []
 }
 
-registry.category("services").add("awesome_dashboard.statistics", StatisticsService);
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
