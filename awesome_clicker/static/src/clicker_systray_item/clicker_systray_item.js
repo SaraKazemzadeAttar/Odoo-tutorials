efc2b4162b94d34e79dcd29d418169bfa8ccd65d
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-27 15:07:48 +0330

    [ADD] action code of client action

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -1,23 +1,35 @@
 import {registry} from "@web/core/registry";
-import { Component , useState , useExternalListener} from "@odoo/owl";
+import {Component, useExternalListener, useState} from "@odoo/owl";
 import {useService} from "@web/core/utils/hooks";
-import { ClientAction} from "../client_action/client_action";
+import {ClientAction} from "../client_action/client_action";
 
 class ClickerSystray extends Component {
 	static template = "awesome_clicker.ClickerSystray";
 	static props = {};
+	static components = {ClientAction}
 
 	setup() {
 		this.state = useState({counter: 0});
 		this.action = useService("action")
-        useExternalListener(document.body, "click", () => this.state.counter++, true);
-    }
+		useExternalListener(document.body, "click", () => this.state.counter++, true);
+	}
 
 	increment() {
 		this.state.counter += 9;
 	}
+
+	openClientAction() {
+		this.action.doAction({
+				type: "ir.actions.client",
+				tag: "awesome_clicker.client_action",
+				target: "new",
+				name: "Clicker"
+			}
+		)
+	}
 }
-export const systrayItem ={
+
+export const systrayItem = {
 	Component: ClickerSystray
 }
 registry.category("systray").add("awesome_clicker.ClickerSystray", systrayItem, {sequence: 1000})
