79d087b35a7ec019acbcb94a677ce6a2835d1aee
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-27 15:34:04 +0330

    [ADD] clicker_service

---
+++ awesome_clicker/static/src/clicker_service/clicker_service.js
@@ -0,0 +1,18 @@
+import {rpc} from "@web/core/network/rpc";
+import {reactive} from "@odoo/owl";
+import {registry} from "@web/core/registry";
+
+export const ClickerService = {
+	start() {
+		const state = reactive({ clicker: 0});
+		function increment(inc){
+			state.clicker += inc;
+		}
+		document.addEventListener("click", () => increment(1), true);
+		return {
+			state,
+			increment,
+		};
+	},
+};
+registry.category("services").add("awesome_clicker.clicker", ClickerService);
