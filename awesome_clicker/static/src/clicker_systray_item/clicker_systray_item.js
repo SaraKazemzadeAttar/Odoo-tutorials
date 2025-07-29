8fe11a5816015bf885540fcebabf89ca88985ca5
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-29 09:25:16 +0330

    [FIX] the typo ,click_value directory instead of clicker|_value

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -3,7 +3,7 @@ import {Component, useState} from "@odoo/owl";
 import {useService} from "@web/core/utils/hooks";
 import {ClientAction} from "../client_action/client_action";
 import { useClicker } from "../clicker_hook"
-import {ClickValue} from "../clicker_value/click_value";
+import {ClickValue} from "../click_value/click_value";
 
 class ClickerSystray extends Component {
 	static template = "awesome_clicker.ClickerSystray";
--- awesome_clicker/static/src/client_action/client_action.js
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -2,7 +2,7 @@ import {registry} from "@web/core/registry";
 import { Component , useState , useExternalListener, openClientAction} from "@odoo/owl";
 import {useService} from "@web/core/utils/hooks";
 import { useClicker } from "../clicker_hook"
-import { ClickValue} from "../clicker_value/click_value";
+import { ClickValue} from "../click_value/click_value";
 
 export class ClientAction extends Component {
 	static template = "awesome_clicker.client_action";
