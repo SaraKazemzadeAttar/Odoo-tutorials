1e98a10de4b60dc3fde01e69bf80d672b14c6856
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-29 12:53:14 +0330

    [REM] clicker_service.js: Rewrite the clicker service to instantiate and export the clicker model class.

--- awesome_clicker/static/src/clicker_service/clicker_service.js
+++ awesome_clicker/static/src/clicker_service/clicker_service.js
@@ -1,42 +1,17 @@
-import {rpc} from "@web/core/network/rpc";
-import {reactive} from "@odoo/owl";
 import {registry} from "@web/core/registry";
+import {ClickerModel} from "../clicker_model";
 
 export const ClickerService = {
 	start() {
-		const state = reactive({clicks:900, level:0, clickBots: 0});
+		const clicker_model = new ClickerModel();
+		document.addEventListener("click", () => clicker_model.addClick(), true);
 
-        setInterval(() => {
-            if (state.clickBots > 0) {
-                state.clicks += 10 * state.clickBots;
-                checkMilestones();
-            }
-        }, 10000);
+		setInterval(() => {
+			clicker_model.tick()
+		}, 10000);
 
-
-	    function increment(inc) {
-	        state.clicks += inc;
-	        checkMilestones();
-	    }
-
-	    function checkMilestones() {
-	        if (state.level === 0 && state.clicks >= 1000) {
-	            state.level = 1;
-	        }
-	    }
-
-	    function buyClickBot() {
-	        if (state.clicks >= 1000) {
-	            state.clicks-= 1000;
-	            state.clickBots += 1;
-	        }
-	    }
-		document.addEventListener("click", () => increment(1), true);
-		return {
-			state,
-			increment,
-			buyClickBot,
-		};
-	},
+		return clicker_model;
+	}
 };
+
 registry.category("services").add("awesome_clicker.clicker", ClickerService);
