0e9ee0eb13cad06e905a676e3ba52be00f544a42
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-30 17:35:58 +0330

    [ADD] gallery_controller.* , gallery_arch_parser.js: load some data

--- awesome_gallery/static/src/gallery_arch_parser.js
+++ awesome_gallery/static/src/gallery_arch_parser.js
@@ -1,8 +1,10 @@
-    export class GalleryArchParser {
-    parse(xmlDoc) {
-       const imgField = xmlDoc.getAttribute("image_field")
-       return {
-           imgField,
-       }
-    }
+export class GalleryArchParser {
+	parse(xmlDoc) {
+		const image_field = xmlDoc.getAttribute("image_field")
+		const limit = xmlDoc.getAttribute("limit") || 80;
+		return {
+			image_field,
+			limit,
+		}
+	}
 }
