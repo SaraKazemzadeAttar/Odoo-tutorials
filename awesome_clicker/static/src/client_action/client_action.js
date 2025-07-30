9f6cd86d23895e3c05c5bcbd7ddbf341b9b5e500
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-30 13:05:24 +0330

    [ADD] client_action.*: add Notebook component

--- awesome_clicker/static/src/client_action/client_action.js
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -2,11 +2,12 @@ import {registry} from "@web/core/registry";
 import { Component , useState , useExternalListener, openClientAction} from "@odoo/owl";
 import { useClicker } from "../clicker_hook"
 import { ClickValue} from "../click_value/click_value";
+import { Notebook } from "@web/core/notebook/notebook";
 
 export class ClientAction extends Component {
 	static template = "awesome_clicker.client_action";
 	static props = ["*"];
-	static components= {ClickValue}
+	static components= {ClickValue, Notebook}
 
     setup() {
         this.clicker = useClicker()
