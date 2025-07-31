72382dd0f144715e6c4e6908105811c5b6fb32f6
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-31 10:42:25 +0330

    [FIX] gallery_arch_parser.js: rename a variable

--- awesome_gallery/static/src/gallery_arch_parser.js
+++ awesome_gallery/static/src/gallery_arch_parser.js
@@ -1,9 +1,9 @@
 export class GalleryArchParser {
 	parse(xmlDoc) {
-		const image_field = xmlDoc.getAttribute("image_field")
+		const imageField = xmlDoc.getAttribute("image_field")
 		const limit = xmlDoc.getAttribute("limit") || 80;
 		return {
-			image_field,
+			imageField,
 			limit,
 		}
 	}
