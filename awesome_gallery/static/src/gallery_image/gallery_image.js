276073e8029c5442a7a9bb7af13495e78c6693d6
Sara K. Atar <Sara.atar2004@gmail.com>
2025-07-31 12:15:39 +0330

    [ADD] gallery_image.*:  Switch to form view on click

--- awesome_gallery/static/src/gallery_image/gallery_image.js
+++ awesome_gallery/static/src/gallery_image/gallery_image.js
@@ -1,6 +1,7 @@
 import {Component} from "@odoo/owl";
 import {GalleryModel} from "../gallery_model";
 import {url} from "@web/core/utils/urls";
+import { useService } from "@web/core/utils/hooks";
 
 export class GalleryImage extends Component {
 	static template = "awesome_gallery.GalleryImage";
@@ -8,8 +9,16 @@ export class GalleryImage extends Component {
 		record: Object,
 		model: GalleryModel
 	};
-// Getter method to compute the image URL based on the record and model
-	get imageUrl() {
+	setup(){
+		this.action = useService("action");
+	}
+	onImageClick() {
+	    this.action.switchView("form");  // switches current view to form view mode
+	}
+
+
+	// Getter method to compute the image URL based on the record and model
+	get getimageUrl() {
 		return url("/web/image", {
 			model: this.props.model.resModel,
 			id: this.props.record.id,
