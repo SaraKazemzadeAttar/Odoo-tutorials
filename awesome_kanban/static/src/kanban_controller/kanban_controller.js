0a53a2d2ffeea1d04403fe6f893311fe054980cc
Sara K. Atar <Sara.atar2004@gmail.com>
2025-08-02 12:50:25 +0330

    [ADD] add filtering by customers in view

--- awesome_kanban/static/src/kanban_controller/kanban_controller.js
+++ awesome_kanban/static/src/kanban_controller/kanban_controller.js
@@ -1,15 +1,30 @@
-import { KanbanController } from "@web/views/kanban/kanban_controller";
-import { CustomerList } from "../customer_list/customer_list";
+import {KanbanController} from "@web/views/kanban/kanban_controller";
+import {CustomerList} from "../customer_list/customer_list";
 
 export class AwesomeKanbanController extends KanbanController {
-	static components = { ...KanbanController.components , CustomerList };
+	static components = {...KanbanController.components, CustomerList};
 	static template = "awesome_kanban.AwesomeKanbanController";
 
-	setup(){
+	setup() {
 		super.setup();
+		this.searchKey = Symbol("isFromAwesomeKanban");
 	}
 
-	selectCustomer(customer_ID){
-		console.log(customer_ID)
-}
+	selectCustomer(partner_id , partner_name) {
+		const customerFilters = this.env.searchModel.getSearchItems((searchItem) =>
+			searchItem[this.searchKey]
+		);
+
+		// Make sure that by clicking on a customer, the old filter is replaced by the new one.
+		for (const customerFilter of customerFilters) {
+			if (customerFilter.isActive) {
+				this.env.searchModel.toggleSearchItem(customerFilter.id); // turns it off
+			}
+		}
+		this.env.searchModel.createNewFilters([{
+			description: partner_name,
+			domain: [["partner_id", "=", partner_id]],
+			[this.searchKey]: true,
+		}])
+	}
 }
