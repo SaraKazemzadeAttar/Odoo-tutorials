73c3df7e0b980363705af244cde74e64098ab13c
Sara K. Atar <Sara.atar2004@gmail.com>
2025-08-02 15:08:48 +0330

    [ADD] customer_list.* : add paginate customers

--- awesome_kanban/static/src/customer_list/customer_list.js
+++ awesome_kanban/static/src/customer_list/customer_list.js
@@ -2,6 +2,7 @@ import {Component, onWillStart, useState} from "@odoo/owl";
 import {useService} from "@web/core/utils/hooks";
 import {KeepLast} from "@web/core/utils/concurrency";
 import {fuzzyLookup} from "@web/core/utils/search";
+import {Pager} from "@web/core/pager/pager"
 
 export class CustomerList extends Component {
 	static template = "awesome_kanban.CustomerList";
@@ -10,34 +11,47 @@ export class CustomerList extends Component {
 			type: Function,
 		},
 	};
+	static components = {Pager};
 
 	setup() {
 		this.orm = useService("orm");
 		this.keepLast = new KeepLast();
 		this.partners = useState({data: []});
 		this.state = useState({
-            searchString: "",
-            displayActiveCustomers: false,
-        })
+			searchString: "",
+			displayActiveCustomers: false,
+		})
+		this.pager = useState({offset: 0, limit: 20});
 
 		onWillStart(async () => {
-			this.partners.data = await this.loadCustomers([]);
+			const {length, records} = await this.loadCustomers();
+			this.partners.data = records;
+			this.pager.total = length;
 		})
 	}
 
-	get displayedPartners(){
+	get displayedPartners() {
 		return this.filterCustomers(this.state.searchString);
 	}
 
 
 	async onChangeActiveCustomers(ev) {
 		this.state.displayActiveCustomers = ev.target.checked;
-		this.partners.data = await this.keepLast.add(this.loadCustomers());
+		const {length, records} = await this.keepLast.add(this.loadCustomers());
+		this.partners.data = records;
+		this.pager.total = length;
 	}
 
 	loadCustomers() {
+		const {limit, offset} = this.pager;
 		const domain = this.state.displayActiveCustomers ? [["opportunity_ids", "!=", false]] : [];
-		return this.orm.searchRead("res.partner", domain, ["display_name"]);
+        return this.orm.webSearchRead("res.partner", domain, {
+            specification: {
+                "display_name": {},
+            },
+            limit,
+            offset,
+        })
 	}
 
 
@@ -51,5 +65,12 @@ export class CustomerList extends Component {
 		} else {
 			return this.partners.data;
 		}
+	}
+
+	async onUpdatePager(newState) {
+		Object.assign(this.pager, newState);
+		const {records} = await this.loadCustomers();
+		this.partners.data = records;
+		this.filterCustomers(this.filterName);
 	}
 }
