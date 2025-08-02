1169120cac091adce3cb71ea2bf5cce20d56b4c0
Sara K. Atar <Sara.atar2004@gmail.com>
2025-08-02 12:02:58 +0330

    [ADD] create a customer_list component

--- awesome_kanban/static/src/awesome_kanban_view.js
+++ awesome_kanban/static/src/awesome_kanban_view.js
@@ -1,7 +1,7 @@
 /** @odoo-module */
 import { kanbanView } from "@web/views/kanban/kanban_view";
 import { registry } from '@web/core/registry';
-import { AwesomeKanbanController} from "./kanban_controller";
+import { AwesomeKanbanController} from "./kanban_controller/kanban_controller";
 
 const awesomeKanbanController = {
 	...kanbanView,
